38691b1e6da68bb20f2fcd451c9b119d4d2ef6e4
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2022-12-27 18:04:52 -0300

    responsive footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,14 +4,14 @@ import instagram from '../assets/instagram.svg'
 
 const Footer = () => {
     return(
-        <div className="grid grid-cols-3 gap-4 px-4 justify-center h-14 bg-gradient-to-r from-cyan-500 to-blue-500">
+        <div className="grid sm:grid-cols-3 lg:grid-cols-3 gap-4 px-4 justify-center h-auto bg-gradient-to-r from-cyan-500 to-blue-500">
             <div>
                 <h1 className="font-sans h-full font-bold leading-loose text-stone-50 font-sans text-2xl ">borzonedaniel@gmail.com </h1>
             </div>
             <div>
             
             </div>
-            <div className="text-stone-50 text-4xl grid justify-end items-center">
+            <div className="text-stone-50 text-4xl grid justify-center lg:justify-end items-center">
                 <Link href="https://www.instagram.com/nextart.gallery/" class="from-cyan-500 to-blue-500 cursor-pointer">
                 <Image
                     src={instagram}
